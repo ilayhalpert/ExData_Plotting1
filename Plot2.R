728e0675e8972214bd0d7854fa2c9a2513df5a5f
ilayhalpert <ilayhalpert@users.noreply.github.com>
2015-06-03 23:16:54 +0300

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,4 +1,12 @@
-## The data will be convert the sae way as Plot1
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
 
 plot(data$Global_active_power~data$Datetime, type="l",
      ylab="Global Active Power (kilowatts)", xlab="")
